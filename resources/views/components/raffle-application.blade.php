5506a422f7b5dacc96227f4b8fef21e43b196e3d
Ana Claudia <anacnogueira@gmail.com>
2026-08-13 11:21:26 -0300

    Utilizando wire:model

--- resources/views/components/raffle-application.blade.php
+++ resources/views/components/raffle-application.blade.php
@@ -3,13 +3,14 @@
 use Livewire\Component;
 
 new class extends Component {
-    //
+    public ?string $email = null;
 };
 ?>
 
 <div>
-    <form action="#">
-        <x-ui.input label="Enter your email" name="email" />
+    {{ $email }}
+    <form wire:submit="$refresh">
+        <x-ui.input wire:model="email" label="Enter your email" name="email" />
         <x-ui.button type="submit" class="mt-4">Submit</x-ui.button>
     </form>
 </div>
