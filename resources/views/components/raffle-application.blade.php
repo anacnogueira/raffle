7d56cc176acb33cc62cd326844ab76b2de2682fd
Ana Claudia <anacnogueira@gmail.com>
2026-08-13 11:41:21 -0300

    Adiciona metodo para salvar dados do form

--- resources/views/components/raffle-application.blade.php
+++ resources/views/components/raffle-application.blade.php
@@ -4,13 +4,26 @@ use Livewire\Component;
 
 new class extends Component {
     public ?string $email = null;
+    public bool $success = false;
+
+    public function save()
+    {
+        // cria o registro
+        $this->success = true;
+    }
 };
 ?>
 
 <div>
-    {{ $email }}
-    <form wire:submit="$refresh">
-        <x-ui.input wire:model="email" label="Enter your email" name="email" />
-        <x-ui.button type="submit" class="mt-4">Submit</x-ui.button>
-    </form>
+    @if ($success)
+        <div class="flex flex-col items-center justify-center p-4 bg-green-100 border-1 rounded-lg border-green-300">
+            <h1 class="text-2xl font-bold">Thank you for your submisstion</h1>
+            <p class="mt-2">We will contact you soon.</p>
+        </div>
+    @else
+        <form wire:submit="save">
+            <x-ui.input wire:model="email" label="Enter your email" name="email" />
+            <x-ui.button type="submit" class="mt-4">Submit</x-ui.button>
+        </form>
+    @endif
 </div>
