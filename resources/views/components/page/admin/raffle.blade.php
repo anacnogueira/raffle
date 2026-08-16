a6cbaf8c108350de5818ba31b5bb26699756da07
Ana Claudia <anacnogueira@gmail.com>
2026-08-16 10:39:56 -0300

    Orgaizando componentes Livewire

---
+++ resources/views/components/page/admin/raffle.blade.php
@@ -0,0 +1,12 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component {
+    //
+};
+?>
+
+<div>
+    Admin Aqui
+</div>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::livewire('/login', 'auth/login')->name('login');
+Route::livewire('/', 'raffle-application')->name('home');
+
 
 Route::middleware('auth')->group(function() {
-    Route::livewire('/', 'raffle-application')->name('home');
+     Route::livewire('/admin/raffle', 'page/admin/raffle')->name('admin.raffle');
 });
