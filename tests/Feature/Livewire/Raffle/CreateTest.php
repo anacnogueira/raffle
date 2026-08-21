6c199c47381d5afa00f20b6181126519b3a3eecf
Ana Claudia <anacnogueira@gmail.com>
2026-08-21 14:28:23 -0300

    Testando criação de um sorteio

--- tests/Feature/Livewire/Raffle/CreateTest.php
+++ tests/Feature/Livewire/Raffle/CreateTest.php
@@ -1,5 +1,18 @@
 <?php
 
-it('example', function() {
-    expect(true)->toBeTrue();
+use Livewire\Livewire;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+it('should create a new raffle', function() {
+    Livewire::test('raffle.create')
+        ->set('name', 'Test Raffle')
+        ->call('handle')
+        ->assertDispatched('raffle::refresh')
+        ->assertSet('name', '');
+
+    $this->assertDatabaseHas('raffles', [
+        'name' => 'Test Raffle'
+    ]);
 });
