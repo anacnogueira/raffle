a1ecfe898d05484afe77338e3c3886a8fbca3c15
Ana Claudia <anacnogueira@gmail.com>
2026-08-22 20:38:18 -0300

    Arruma o faker

--- database/factories/RaffleFactory.php
+++ database/factories/RaffleFactory.php
@@ -19,7 +19,7 @@ class RaffleFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->sentence(3),
+            'name' => $this->faker->sentence(3),
             'published_at' => $this->faker->optional(0.7)->dateTimeBetween('-1 year', '+1 year')
         ];
     }
